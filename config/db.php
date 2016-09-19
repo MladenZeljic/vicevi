c34fb337a39c1a0ea6760270fb8db6904ffa7ed2
MladenZeljic <mladen_zeljic@hotmail.com>
2016-09-19 22:28:14 +0200

    Yii
    Default yii2 files

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=student',
+    'username' => 'root',
+    'password' => 'mladenpriboj93',
+    'charset' => 'utf8',
+];
