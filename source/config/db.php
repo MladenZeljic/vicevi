af8537c0ad6a4d8cbd7463ea6275a06e88a19af1
MladenZeljic <mladen_zeljic@hotmail.com>
2016-09-26 18:54:02 +0200

    database password change

--- source/config/db.php
+++ source/config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=student',
     'username' => 'root',
-    'password' => 'mladenpriboj93',
+    'password' => '',
     'charset' => 'utf8',
 ];
